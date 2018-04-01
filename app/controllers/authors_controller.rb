f91b457a08895cc16942becc2d36a8447301e60b
americanmouths <shiverr@gmail.com>
2018-04-01 12:17:00 -0700

    render index and show with json/html formats

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,17 @@
 class AuthorsController < ApplicationController
   def index
     @authors = Author.alphabatize
-    render json: @authors, status: 200
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @authors }
+    end
   end
 
   def show
     @author = Author.find_by(id: params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @author }
+    end
   end
 end
