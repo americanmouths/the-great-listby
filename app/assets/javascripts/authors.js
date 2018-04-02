a6e5aff666d0a1ee054d450cfcb761c25c1d4b50
americanmouths <shiverr@gmail.com>
2018-04-02 10:10:01 -0700

    create function to show next author with ajax req

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   authorsIndex();
   displayAuthorBooks();
   authorShowPage();
+  showNextAuthor();
 })
 
 //author index - got data onto page - it's not formatted but at least got via ajax
@@ -16,6 +17,13 @@ function authorsIndex(){
   })
 }
 
+// prevent html display from home page link
+function authorHomeLink(){
+  $("#author_index").on('click', function(e){
+    e.preventDefault();
+    authorsIndex();
+  })
+}
 //hide more info about author link on author index until button click
 function hideAuthorLink(){
   $("#author_link").hide()
@@ -27,9 +35,9 @@ function displayAuthorBooks(){
     $("#author_link").show();
     var id = $(this).data("id")
     $.get("/authors/" + id + ".json", function(data){
-      var books = data["books"]
-      var bookTitles = books.map(book => book.title)
-      $("#body-" + id).text(bookTitles)
+      var bookLis = data["books"].map(function (book){
+      return "<li>" + book["title"] + "</li>"}).join('');
+      $("#body-" + id).html(bookLis)
     })
   })
 }
@@ -39,12 +47,24 @@ function authorShowPage(){
   $("a.author_show").on('click', function(){
     $.get(this.href + ".json", function(data){
       var id = data["id"]
-
-      var bookLis = data["books"].map(function (book){
+      var bookLis = data["books"].map(function (book){ //display books
         return "<li>" + book + "</li>"}).join('');
-
         $(".authorName").text(data["name"]);
         $("#books-" + id).html(bookLis)
       })
     })
   }
+
+//function for showing next author on next author click
+function showNextAuthor(){
+  $(".js-next").on("click", function(){
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.get("/authors" + nextId + ".json", function(data){
+      $(".authorName").text(data["name"])
+      var bookLis = data["books"].map(function (book){ //display books
+      return "<li>" + book["title"] + "</li>"}).join('');
+      $("#books-" + nextId).html(bookLis)
+      $(".js-next").attr("data-id", data["id"]); //reset id
+    })
+  })
+}
