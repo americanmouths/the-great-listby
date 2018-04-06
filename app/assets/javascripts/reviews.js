9067e9e20c0fd3db6867a7f36ff12ba67cc506de
americanmouths <shiverr@gmail.com>
2018-04-05 19:46:34 -0700

    add correct ajax path using book id

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,10 +1,15 @@
 $(function (){
   $('form').submit(function(event){
     event.preventDefault();
+
+    var userId = document.getElementById("userId").value
+    var bookId = document.getElementById("bookId").value
+
     var values = $(this).serialize();
-    var posting = $.post('/reviews', values);
+    var posting = $.post('/books/' + bookId + '/reviews', values);
 
     posting.done(function(data){
+
 
     })
   })
