8933791aedc74f07665d30d4eb53e7e6c53cb3a9
americanmouths <shiverr@gmail.com>
2018-04-07 17:20:20 -0700

    Finish ajax/query rendering of all genres

--- app/assets/javascripts/genres.js
+++ app/assets/javascripts/genres.js
@@ -1,4 +1,45 @@
+$(document).ready(function(){
+  genreIndexLink();
+  genreIndex();
+})
+
+//Genre Constructor
+
 function Genre(data){
   this.id = data.id
   this.name = data.name
+}
+
+//Genre Index Prototype
+Genre.prototype.indexTemplate = function(){
+  let genreHTML = `<li><a href="genres/${this.id}"><u>${ this.name }</u></a></li>`
+  return genreHTML
+}
+
+
+//Get Req to Genres
+function genreIndex(){
+  $.getJSON("/genres").done(function(data){
+    appendGenreIndex(data)
+  })
+}
+
+//Fire Ajax when Genre Link clicked in Nav Bar
+function genreIndexLink(){
+  $(document).on('click', 'a.genre_index', function(){
+    $.get("/genres").done(function(){
+      $.getJSON("/genres").done(function(data){
+        appendGenreIndex(data)
+      })
+    })
+  })
+}
+
+//Append Genre Index to Page
+function appendGenreIndex(data){
+  data.forEach(function(genre){
+    let newGenre = new Genre(genre)
+    let genreHTML = newGenre.indexTemplate()
+    $("#genres_container").append(genreHTML)
+  })
 }
